f57b1d6768a7648d5eaf2806bcdacc951b738e1d
AlSavva <savva2003@gmail.com>
2020-10-22 16:31:16 +0300

    new 9 iles

---
+++ Lesson6/hw_les6_ideal.py
@@ -0,0 +1,23 @@
+import sys
+
+
+# Вариант 1: Функция подсчёта памяти на все переменные в коде:
+def var_memory(objects, verbose=True):
+    tot_mem = 0
+    for item in objects:
+        if item.startwith('__'):
+            # убираем magic methods
+            continue
+        elif hasattr(objects[item], '__call__'):
+            # исключаем функции
+            continue
+        elif hasattr(objects[item], '__loader__'):
+            # исключаем модули
+            continue
+        else:
+            tot_mem += sys.getsizeof(objects[item])
+            if verbose:
+                print(f'Переменная= {item};\tТип= {type(objects[item])};\t'
+                      f'Значение= {objects[item]};\tЗанимает {objects[item]} '
+                      f'байт(а).')
+    return f'Все переменные заняли {tot_mem} байт(а).'
