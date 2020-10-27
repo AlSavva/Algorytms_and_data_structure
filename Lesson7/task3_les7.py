c3a6e8dcf0339cfc9f78989eca3526198116133e
AlSavva <savva2003@gmail.com>
2020-10-27 17:43:38 +0300

    added

--- Lesson7/task3_les7.py
+++ Lesson7/task3_les7.py
@@ -5,3 +5,25 @@
 # Примечание: задачу можно решить без сортировки исходного массива. Но если это
 # слишком сложно, используйте метод сортировки, который не рассматривался на
 # уроках (сортировка слиянием также недопустима).
+
+import random
+
+
+def full_randarray(x, y, size):
+    """
+    Возвращает массив из size элементов от x до y в произвольном порядке
+    """
+    array = [random.randint(x, y) for _ in range(size)]
+    random.shuffle(array)
+    return array
+
+def median_search(array):
+    if len(array) == 1:
+        return array[0]
+    else:
+        array.remove(max(array))
+        array.remove(min(array))
+        return median_search(array)
+b = full_randarray(0, 9, 9)
+print(b)
+print(median_search(b))
