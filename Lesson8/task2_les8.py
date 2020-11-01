4c21b8bc00fa3c26879321acd023544ee9cc3fe8
AlSavva <savva2003@gmail.com>
2020-11-01 19:16:14 +0300

    new 9 iles

--- Lesson8/task2_les8.py
+++ Lesson8/task2_les8.py
@@ -13,6 +13,20 @@ graph1 = [
 ]
 
 
+def adj_lst(graph):
+    """
+    :param graph:adjacency matrix of weighted directed graph
+    :return:adjacency list of same graph
+    """
+    adj_list = {}
+    for i in range(len(graph)):
+        adj_list[i] = {}
+        for idx, vol in enumerate(graph[i]):
+            if vol != 0:
+                adj_list[i][idx] = vol
+    return adj_list
+
+
 def dijkstra(graph, start):
     """
     :param graph:graph matrix
@@ -20,27 +34,71 @@ def dijkstra(graph, start):
     :return:short cut from start to any vertex
     """
     length = len(graph)
-    is_visited = [False] * length
+    is_visited = []
     # изначально обозначим стоимость пути до любой точки как беcконечность
-    cost = [float('inf')] * length
-    parent = [-1] * length
-    cost[start] = 0
-    min_cost = 0
-    trans_graph = []
-    way_dict = {}
-    while min_cost < float('inf'):
-        is_visited[start] = True
-        for i, vertex in enumerate(graph[start]):
-            if vertex != 0 and not is_visited[i]:
-                if cost[i] > vertex + cost[start]:
-                    cost[i] = vertex + cost[start]
-                    parent[i] = start
+    costs = {i: float('inf') for i in range(length)}
+    parents = {i: None for i in range(length)}
+    costs[start] = 0
+
+    # is_visited.append(start)
+    def find_low_cost(costs):
         min_cost = float('inf')
-        for i in range(length):
-            if min_cost > cost[i] and not is_visited[i]:
-                min_cost = cost[i]
-                start = i
-    return cost
+        min_cost_node = None
+        for node in costs:
+            cost = costs[node]
+            if cost < min_cost and node not in is_visited:
+                min_cost = cost
+                min_cost_node = node
+        return min_cost_node
+
+    node = find_low_cost(costs)
+    way = {i: [0] for i in range(length)}
+    while node is not None:
+        cost = costs[node]
+        neigh = graph[node]
+        for n in neigh.keys():
+            new_cost = cost + neigh[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        is_visited.append(node)
+        node = find_low_cost(costs)
+    way = {i: [] for i in range(length)}
+    coopy = parents.copy()
+    for key, value in coopy.items():
+        if value is None:
+            if key == start:
+                way[key].append(start)
+            else:
+                way[key].append(None)
+        else:
+            way[key].insert(0, key)
+            while value != start:
+                way[key].insert(0, value)
+                value = coopy[value]
+            way[key].insert(0, start)
+
+
+    return f'{parents}\n{costs}\n{way}'
+
+    # cost[start] = 0
+    # while min_cost < float('inf'):
+    #     is_visited[start] = True
+    #     for i, vertex in enumerate(graph[start]):
+    #         if vertex != 0 and not is_visited[i]:
+    #             if cost[i] > vertex + cost[start]:
+    #                 cost[i] = vertex + cost[start]
+    #                 parent[i] = start
+    #     min_cost = float('inf')
+    #     for i in range(length):
+    #         if min_cost > cost[i] and not is_visited[i]:
+    #             min_cost = cost[i]
+    #             start = i
+    # return cost
+
 
 s = int(input('Enter start vertex: '))
-print(dijkstra(graph1, s))
+graph = adj_lst(graph1)
+print(graph)
+
+print(dijkstra(graph, s))
